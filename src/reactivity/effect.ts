d49311bd6a914177206f3325fe8ae7e8a2328646
ohoherror <1458251589@qq.com>
2023-02-17 10:01:00 +0800

    class

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,43 +1,80 @@
 class ReactiveEffect {
-    private _fn: Function
+    private _fn: any;
+    public scheduler: Function | undefined;
+    deps = []
+    private onStop
     constructor(fn) {
-        this._fn = fn
+        this._fn = fn;
+        // this.scheduler = scheduler;
     }
     run() {
-        activeEffect = this
-        this._fn()
+        activeEffect = this;
+        return this._fn();
+    }
+    stop() {
+        if (this.onStop) {
+            this.onStop()
+        }
+        cleanupEffect(this)
     }
 }
 
-const targetMap = new Map()
-//收集依赖
+function cleanupEffect(effect) {
+    effect.deps.forEach((dep: any) => {
+        dep.delete(effect)
+    });
+}
+const targetMap = new Map();
 export function track(target, key) {
-    let depsMap = targetMap.get(target)
+    // target -> key -> dep
+    let depsMap = targetMap.get(target);
     if (!depsMap) {
-        depsMap = new Map()
-        targetMap.set(target, depsMap)
+        depsMap = new Map();
+        targetMap.set(target, depsMap);
     }
-    let dep = depsMap.get(key)
+
+    let dep = depsMap.get(key);
     if (!dep) {
-        dep = new Set()
-        depsMap.set(key, dep)
+        dep = new Set();
+        depsMap.set(key, dep);
     }
-    dep.add(activeEffect)
+
+    dep.add(activeEffect);
+    activeEffect.deps.push(dep)
 }
-//触发依赖
+
 export function trigger(target, key) {
-    console.log(target)
-    let depsMap = targetMap.get(target)
-    let dep = depsMap.get(key)
+    let depsMap = targetMap.get(target);
+    let dep = depsMap.get(key);
+
     for (const effect of dep) {
-        effect.run()
+        if (effect.scheduler) {
+            effect.scheduler();
+        } else {
+            effect.run();
+        }
     }
-
 }
-let activeEffect
-export function effect(fn: Function) {
-    console.log(fn)
-    let _effect = new ReactiveEffect(fn)
-    _effect.run()
 
+export function stop(runner) {
+    runner.effect.stop()
+}
+
+type effectOptions = {
+    scheduler?: Function;
+    onStop?: any
+};
+
+let activeEffect;
+export function effect(fn, options: effectOptions = {}) {
+    // fn
+    const _effect: any = new ReactiveEffect(fn);
+    Object.assign(_effect, options)
+    _effect.run();
+
+    const runner: any = _effect.run.bind(_effect);
+    runner.effect = _effect
+
+
+    return runner;
 }
