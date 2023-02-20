66c7e24a31d83344d1e0125460db3d5e2c5df152
ohoherror <1458251589@qq.com>
2023-02-20 17:23:32 +0800

    readonly&reactive

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,42 +1,49 @@
+import { track, trigger } from "./effect";
+import { ReactiveFlags } from "./reactive";
 
-import { track, trigger } from "./effect"
+const get = createGetter();
+const set = createSetter();
+const readonlyGet = createGetter(true);
 
 function createGetter(isReadonly = false) {
     return function get(target, key) {
-        const result = Reflect.get(target, key)
-        //收集依赖
-        if (!isReadonly) {
-            track(target, key)
+        if (key === ReactiveFlags.IS_REACTIVE) {
+            return !isReadonly;
+        } else if (key === ReactiveFlags.IS_READONLY) {
+            return isReadonly;
         }
-        return result
-    }
+
+        const res = Reflect.get(target, key);
+
+        if (!isReadonly) {
+            track(target, key);
+        }
+        return res;
+    };
 }
 
 function createSetter() {
-    return function set(target, key, newValue) {
-        const result = Reflect.set(target, key, newValue)
-        //触发依赖
-        trigger(target, key)
-        return result
-    }
+    return function set(target, key, value) {
+        const res = Reflect.set(target, key, value);
+
+        trigger(target, key);
+        return res;
+    };
 }
 
-function readonlySetter() {
-    return function set(target) {
-        console.warn(target + "不能不调用")
-        return true
-    }
-}
-export function mutableHandlers() {
-    return {
-        get: createGetter(),
-        set: createSetter()
-    }
-}
+export const mutableHandlers = {
+    get,
+    set,
+};
 
-export function readonlyHandles() {
-    return {
-        get: createGetter(true),
-        set: readonlySetter()
-    }
-}
+export const readonlyHandlers = {
+    get: readonlyGet,
+    set(target, key) {
+        console.warn(
+            `key :"${String(key)}" set 失败，因为 target 是 readonly 类型`,
+            target
+        );
+
+        return true;
+    },
+};
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,10 +1,28 @@
 
-import { mutableHandlers, readonlyHandles } from './baseHandlers'
+import { mutableHandlers, readonlyHandlers } from './baseHandlers'
+
+
+export const enum ReactiveFlags {
+    IS_REACTIVE = "_v_isReactive",
+    IS_READONLY = "_v_isReadonly"
+}
 
 export function reactive(raw) {
-    return new Proxy(raw, mutableHandlers())
+    return createReactiveObject(raw, mutableHandlers)
 }
 
 export function readonly(raw) {
-    return new Proxy(raw, readonlyHandles())
+    return createReactiveObject(raw, readonlyHandlers)
+}
+
+function createReactiveObject(target, baseHandlers) {
+    return new Proxy(target, baseHandlers)
+}
+
+export function isReadonly(value) {
+    return !!value[ReactiveFlags.IS_READONLY]
+}
+
+export function isReactive(value) {
+    return !!value[ReactiveFlags.IS_REACTIVE]
 }
