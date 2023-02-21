c06f5ed887cf38f0b7bb49db871d2f93bc11e130
ohoherror <1458251589@qq.com>
2023-02-21 16:20:27 +0800

    ref

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from "../effect";
-import { ref } from '../ref'
+import { reactive } from "../reactive";
+import { ref, isRef, unRef } from '../ref'
 
 describe("ref", () => {
     it("happy path", () => {
@@ -25,7 +26,7 @@ describe("ref", () => {
         expect(dummy).toBe(2)
     })
 
-    it.only('should make nested properties reactive', () => {
+    it('should make nested properties reactive', () => {
         const a = ref({
             count: 1
         })
@@ -39,5 +40,22 @@ describe("ref", () => {
         expect(dummy).toBe(2)
         a.value.count = 3
         expect(dummy).toBe(3)
+    })
+
+    it("isRef", () => {
+        const a = ref(1)
+        const user = reactive({
+            age: 1
+        })
+        expect(isRef(a)).toBe(true)
+        expect(isRef(1)).toBe(false)
+        expect(isRef(user)).toBe(false)
+    })
+    it("unRef", () => {
+        const a = ref(1)
+
+        expect(unRef(a)).toBe(1)
+        expect(unRef(1)).toBe(1)
+
     })
 })
