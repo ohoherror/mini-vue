03875074ed300bb1a477a419d919645926efff21
ohoherror <1458251589@qq.com>
2023-03-08 09:26:50 +0800

    compare

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -2,33 +2,77 @@
 import { ShapeFlags } from "../shared/ShapeFlag"
 import { createComponentInstance, setupComponent } from "./component"
 import { createAppAPI } from "./createApp"
-import { Fragment, TextNode } from "./vnode"
+import { Fragment, Text } from "./vnode"
 import { effect } from "../reactivity/effect";
 
 export function createRenderer(options) {
-    const { createElement, patchProp, insert } = options
+    const {
+        createElement: hostCreateElement,
+        patchProp: hostPatchProp,
+        insert: hostInsert,
+    } = options;
+
     function render(vnode, container) {
         patch(vnode, container, null)
     }
 
     function patch(vnode, container, parentComponent) {
-        const { type, shapeFlags } = vnode
+        const { type, shapeFlag } = vnode
         switch (type) {
             case Fragment:
                 processFragment(vnode, container, parentComponent)
                 break
-            case TextNode:
-                processTextNode(vnode, container, parentComponent)
+            case Text:
+                processText(vnode, container, parentComponent)
                 break
             default:
                 //(0001|0101|1001)&0001 >0为true
-                if (shapeFlags & ShapeFlags.ELEMENT) {
+                if (shapeFlag & ShapeFlags.ELEMENT) {
                     processElement(vnode, container, parentComponent)
-                } else if (shapeFlags & ShapeFlags.STATEFUL_COMPONENT) {
+                } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
                     processComponent(vnode, container, parentComponent)
                 }
                 break
         }
+    }
+    function processText(vnode: any, container: any, parentComponent) {
+        const element = (vnode.el = document.createTextNode(vnode.children))
+        container.appendChild(element)
+    }
+
+    function processFragment(vnode: any, container: any, parentComponent) {
+        mountChildren(vnode, container, parentComponent)
+    }
+
+    function processElement(vnode: any, container: any, parentComponent) {
+        mountElement(vnode, container, parentComponent)
+    }
+
+    function mountElement(vnode: any, container: any, parentComponent) {
+        //type里面放标签名，如：div,button等
+        //props里面放样式、事件等
+        //children里面放内容
+        //此时的shapeFlags只有字符串类型或数组类型
+        const el = (vnode.el = hostCreateElement(vnode.type));
+        const { children, shapeFlag } = vnode;
+        if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
+            el.textContent = children
+        } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
+            mountChildren(vnode, el, parentComponent)
+        }
+        const { props } = vnode;
+        for (const prop in props) {
+            hostPatchProp(el, prop, props)
+        }
+
+        hostInsert(el, container)
+    }
+
+
+    function mountChildren(vnode: any, container: any, parentComponent) {
+        vnode.children.forEach((v) => {
+            patch(v, container, parentComponent);
+        });
     }
 
     function processComponent(vnode, container, parentComponent) {
@@ -46,52 +90,18 @@ export function createRenderer(options) {
     function setupRenderEffect(instance, initialVNode, container) {
         //this指向setup里面的对象
         effect(() => {
+            console.log("init");
             const { proxy } = instance;
             const subTree = (instance.subTree = instance.render.call(proxy));
-            console.log(subTree)
+
             patch(subTree, container, instance);
 
             initialVNode.el = subTree.el;
 
+            instance.isMounted = true;
+
         })
 
-    }
-
-    function processElement(vnode: any, container: any, parentComponent) {
-        mountElement(vnode, container, parentComponent)
-    }
-
-    function processFragment(vnode: any, container: any, parentComponent) {
-        mountChildren(vnode, container, parentComponent)
-    }
-
-    function processTextNode(vnode: any, container: any, parentComponent) {
-        const element = (vnode.el = document.createTextNode(vnode.children))
-        container.appendChild(element)
-    }
-
-    function mountElement(vnode: any, container: any, parentComponent) {
-        //type里面放标签名，如：div,button等
-        //props里面放样式、事件等
-        //children里面放内容
-        //此时的shapeFlags只有字符串类型或数组类型
-        const { type: domElType, props, children, shapeFlags } = vnode
-        const domEl = (vnode.el = createElement(domElType))
-        for (const prop in props) {
-            patchProp(domEl, prop, props)
-        }
-        if (shapeFlags & ShapeFlags.TEXT_CHILDREN) {
-            domEl.textContent = children
-        } else if (shapeFlags & ShapeFlags.ARRAY_CHILDREN) {
-            mountChildren(vnode, domEl, parentComponent)
-        }
-        insert(domEl, container)
-    }
-
-    function mountChildren(vnode: any, container: any, parentComponent) {
-        vnode.children.forEach((v) => {
-            patch(v, container, parentComponent);
-        });
     }
 
     return {
